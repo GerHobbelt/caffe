0c75417310ce4e4a14a7bdc29028bf16252ac0b1
astanwar99 <ashwani@doselect.com>
2021-04-22 18:24:00 +0530

    Fix bugs

--- data/VOC0712/create_data.sh
+++ data/VOC0712/create_data.sh
@@ -6,7 +6,7 @@ cd $root_dir
 redo=1
 data_root_dir="$HOME/data/train"
 dataset_name="Custom"
-mapfile="$HOME/data/labelmap_voc.prototxt"
+mapfile="$HOME/data/labelmap.prototxt"
 anno_type="detection"
 db="lmdb"
 min_dim=200
@@ -21,7 +21,7 @@ then
 fi
 for subset in test trainval
 do
-  python $root_dir/scripts/create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $root_dir/FaceDetection/$subset.txt $data_root_dir/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
+  python3 $root_dir/scripts/create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $root_dir/FaceDetection/$subset.txt $data_root_dir/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
 #  echo $anno_type $mapfile $min_dim $max_dim $width $height $extra_cmd $data_root_dir $root_dir/FaceDetection/$subset.txt $data_root_dir/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
 
 done
