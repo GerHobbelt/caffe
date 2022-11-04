4740e07fc464d330d3f9cb6023bb58a60006419b
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-11-04 12:25:24 +0100

    (CI)(python) drop python2 support

--- scripts/travis/install-python-deps.sh
+++ scripts/travis/install-python-deps.sh
@@ -5,11 +5,6 @@
 BASEDIR=$(dirname $0)
 source $BASEDIR/defaults.sh
 
-if ! $WITH_PYTHON3 ; then
-  # Python2
-  :
-else
-  # Python3
-  pip install --pre protobuf==3.0.0b3
-  pip install pydot
-fi
+# Python3
+pip install --pre protobuf==3.0.0b3
+pip install pydot
