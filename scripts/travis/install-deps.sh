a122e57da4a988e03188e3b7104eaae8993c891a
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-11-04 12:31:12 +0100

    (CI) don't build protobuf from source

--- scripts/travis/install-deps.sh
+++ scripts/travis/install-deps.sh
@@ -30,35 +30,6 @@ apt-get install -y --no-install-recommends \
   python3-numpy \
   python3-skimage
 
-  # build Protobuf3 since it's needed for Python3
-  PROTOBUF3_DIR=~/protobuf3
-  pushd .
-  if [ -d "$PROTOBUF3_DIR" ] && [ -e "$PROTOBUF3_DIR/src/protoc" ]; then
-    echo "Using cached protobuf3 build ..."
-    cd $PROTOBUF3_DIR
-  else
-    echo "Building protobuf3 from source ..."
-    rm -rf $PROTOBUF3_DIR
-    mkdir $PROTOBUF3_DIR
-
-    # install some more dependencies required to build protobuf3
-    apt-get install -y --no-install-recommends \
-      curl \
-      dh-autoreconf \
-      unzip
-
-    wget https://github.com/google/protobuf/archive/3.0.x.tar.gz -O protobuf3.tar.gz
-    tar -xzf protobuf3.tar.gz -C $PROTOBUF3_DIR --strip 1
-    rm protobuf3.tar.gz
-    cd $PROTOBUF3_DIR
-    ./autogen.sh
-    ./configure --prefix=/usr
-    make --jobs=$NUM_THREADS
-  fi
-  make install
-  popd
-fi
-
 if $WITH_IO ; then
   apt-get install -y --no-install-recommends \
     libleveldb-dev \
