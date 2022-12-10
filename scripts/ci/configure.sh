091c6c5b9cf25f36ba2807842ca87ca17abb612f
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-12-10 20:47:18 +0100

    (actions) log env vars

--- scripts/ci/configure.sh
+++ scripts/ci/configure.sh
@@ -4,6 +4,11 @@
 BASEDIR=$(dirname $0)
 source $BASEDIR/defaults.sh
 
+echo -e "\e[35m\e[1mWITH_CMAKE: ${WITH_CMAKE}\e[0m"
+echo -e "\e[35m\e[1mWITH_IO:    ${WITH_IO}\e[0m"
+echo -e "\e[35m\e[1mWITH_CUDA:  ${WITH_CUDA}\e[0m"
+echo -e "\e[35m\e[1mWITH_CUDNN: ${WITH_CUDNN}\e[0m"
+
 if ! $WITH_CMAKE ; then
   source $BASEDIR/configure-make.sh
 else
