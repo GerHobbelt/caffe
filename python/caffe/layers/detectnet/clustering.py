e443ee312e06c9cb42960915efa3f906c7886eba
MichalZajacMTS <127204802+MichalZajacMTS@users.noreply.github.com>
2023-04-03 09:26:38 +0200

    receptive field of view for clustering was not updating according to input images size - fixed

--- python/caffe/layers/detectnet/clustering.py
+++ python/caffe/layers/detectnet/clustering.py
@@ -150,7 +150,8 @@ def gridbox_to_boxes(net_cvg, net_boxes, self):
     cell_width = im_sz_x / grid_sz_x
     cell_height = im_sz_y / grid_sz_y
 
-    cvg_val = net_cvg[0, 0:grid_sz_y, 0:grid_sz_x]
+    #cvg_val = net_cvg[0, 0:grid_sz_y, 0:grid_sz_x]
+    cvg_val = net_cvg[0][0:net_cvg.shape[1],0:net_cvg.shape[2]]
 
     if (self.is_groundtruth):
         mask = (cvg_val > 0)
