f08e5f22c133664382d88114fe85782935652a03
Sue Ann Koay <koays@janelia.hhmi.org>
2023-07-02 18:37:06 -0400

    Now can identify GPU devices using their PCI bus ID.

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -1,6 +1,9 @@
 from .pycaffe import Net, SGDSolver, NesterovSolver, AdaGradSolver, RMSPropSolver, AdaDeltaSolver, AdamSolver, NCCL, Timer
-from ._caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, device_query, Layer, get_solver,\
-    layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
+from ._caffe import (
+    init_log, log, set_mode_cpu, set_mode_gpu, set_device, device_query, find_device, find_device_by_pci_bus,
+    Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank,
+    set_multiprocess, has_nccl
+)
 from ._caffe import __version__
 from .proto.caffe_pb2 import TRAIN, TEST
 from .classifier import Classifier
